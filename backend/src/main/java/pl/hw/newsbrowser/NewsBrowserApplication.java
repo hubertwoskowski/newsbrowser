323f12593fe30bd7a2397a3f710c05cf622536ef
Hubert Wośkowski <hubertwoskowski@gmail.com>
2020-02-07 20:14:42 +0100

    Initial commit

---
+++ backend/src/main/java/pl/hw/newsbrowser/NewsBrowserApplication.java
@@ -0,0 +1,13 @@
+package pl.hw.newsbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsBrowserApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/hw/newsbrowser/NewsBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hw.newsbrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
