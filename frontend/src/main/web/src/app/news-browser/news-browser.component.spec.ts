8159d4d511a25fa397c36ca3b9f94c837e732407
Hubert Wośkowski <hubertwoskowski@gmail.com>
2020-02-17 11:35:48 +0100

    News browser test

--- frontend/src/main/web/src/app/news-browser/news-browser.component.spec.ts
+++ frontend/src/main/web/src/app/news-browser/news-browser.component.spec.ts
@@ -3,6 +3,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ArticleTileComponent} from '../article-tile/article-tile.component';
 import {LowResolutionMessageComponent} from '../low-resolution-message/low-resolution-message.component';
+import {Article, News} from '../model/news.interface';
 import {NewsBrowserComponent} from './news-browser.component';
 
 describe('NewsBrowserComponent', () => {
@@ -26,10 +27,34 @@ describe('NewsBrowserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewsBrowserComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.news = newsMock;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should compare component with expected values', () => {
+    fixture.detectChanges();
+    let title = fixture.nativeElement.querySelector('.browser__title');
+    expect(title.innerText).toEqual('Przeglądarka newsów');
+    let tile = fixture.nativeElement.querySelector('.tile');
+    expect(tile.innerText).toBeDefined();
+  });
 });
+
+const articleMock: Article = {
+  author: 'author',
+  title: 'title',
+  description: 'description',
+  date: '2020-01-01',
+  sourceName: 'sourceName',
+  articleUrl: 'http://article-url/',
+  imageUrl: 'http://image-url/'
+};
+
+const newsMock: News = {
+  country: 'pl',
+  category: 'category',
+  articles: [articleMock]
+};
